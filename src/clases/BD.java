178d000da0ff51350a15ba7274711904b63fa26b
irving477 <83791172+irving477@users.noreply.github.com>
2021-05-27 20:55:13 -0500

    actualizacion_27_05_2021

--- src/clases/BD.java
+++ src/clases/BD.java
@@ -1,11 +1,11 @@
 
 package clases;
 
-import java.beans.Statement;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.sql.Connection;//interface
+import java.sql.DriverManager;//clase
 import java.sql.SQLException;
+import java.sql.Statement;//interface
+import java.sql.ResultSet;//interface
 
 
 
@@ -19,4 +19,60 @@ public class BD {
     String coneccion = "jdbc:mysql://localhost/hotel?user=root&password=";
     conn = DriverManager.getConnection(coneccion);
     }
+    
+    public void Consult(String query){
+        //information_schema
+        try {
+            stmt =  conn.createStatement();
+            rs = stmt.executeQuery(query); //envia una consulta devuelve un objeto ResultSet para su implementacion
+            if (rs!=null) {
+                rs = stmt.getResultSet(); //obtiene los resultados
+                //se coloca sobre el primer registro
+               // rs.first();
+            }
+        }
+        catch (SQLException ex){
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("Error: " + ex.getErrorCode());
+        }
+    }
+    public int Update(String query){
+        //information_schema
+        int rModif=0;
+        try {
+            stmt = conn.createStatement();
+             rModif= stmt.executeUpdate(query);
+        }
+        catch (SQLException ex){
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("Error: " + ex.getErrorCode());
+        }
+        return rModif;
+    }
+
+
+    public void closeRsStmt(){
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException sqlEx) { } // ignore
+            rs = null;
+        }
+        if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) { } // ignore
+                stmt = null;
+         }
+    }
+    
+    public void closeConnection() {
+         try {
+               if (conn!=null)  conn.close();
+            } catch (SQLException sqlEx) { } // ignore
+            rs = null;
+    }
+  
 }
