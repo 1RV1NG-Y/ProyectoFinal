b2e5770558fd88538cf88a99336070a007764f3b
AdanSkandar <83788158+AdanSkandar@users.noreply.github.com>
2021-06-12 11:30:48 -0500

    Registro Captura datos falta conexion

--- src/proyectofinal/RegistroHuespedes.java
+++ src/proyectofinal/RegistroHuespedes.java
@@ -8,6 +8,7 @@ package proyectofinal;
 import clases.BD;
 import java.awt.Cursor;
 import java.sql.SQLException;
+import java.util.Date;
 import javax.swing.ImageIcon;
 import javax.swing.JRadioButton;
 
@@ -31,6 +32,7 @@ public class RegistroHuespedes extends javax.swing.JFrame {
         this.setLocationRelativeTo(null);
         this.setUndecorated(true);
         this.jButtonElegHabitacion.setCursor(new Cursor(HAND_CURSOR));
+        this.jButtonRegistrar.setCursor(new Cursor(HAND_CURSOR));
     }
 
     /**
@@ -59,6 +61,7 @@ public class RegistroHuespedes extends javax.swing.JFrame {
         jTextFieldElegHabitacion = new javax.swing.JTextField();
         jLabelElegHabitacion = new javax.swing.JLabel();
         jButtonElegHabitacion = new javax.swing.JButton();
+        jButtonRegistrar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMaximumSize(new java.awt.Dimension(744, 408));
@@ -120,15 +123,24 @@ public class RegistroHuespedes extends javax.swing.JFrame {
 
         jButtonElegHabitacion.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         jButtonElegHabitacion.setText("Seleccionar habitacion");
+        jButtonElegHabitacion.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jButtonElegHabitacionMouseReleased(evt);
+            }
+        });
+
+        jButtonRegistrar.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jButtonRegistrar.setText("Registrar");
+        jButtonRegistrar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jButtonRegistrarMouseReleased(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(178, 178, 178)
-                .addComponent(jLabel2)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(25, 25, 25)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -142,7 +154,7 @@ public class RegistroHuespedes extends javax.swing.JFrame {
                                 .addComponent(jLabelCiud)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(jTextFieldCiudadHuesped)))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 247, Short.MAX_VALUE)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(jRadioButtonWoman, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jRadioButtonMan, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -161,12 +173,24 @@ public class RegistroHuespedes extends javax.swing.JFrame {
                                     .addComponent(jLabelElegHabitacion, javax.swing.GroupLayout.Alignment.TRAILING))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(jTextFieldElegHabitacion)))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButtonElegHabitacion)
-                        .addGap(0, 106, Short.MAX_VALUE)))
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabelFoto, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(28, 28, 28))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(178, 178, 178)
+                        .addComponent(jLabel2))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(290, 290, 290)
+                        .addComponent(jButtonRegistrar)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                    .addContainerGap(323, Short.MAX_VALUE)
+                    .addComponent(jButtonElegHabitacion)
+                    .addGap(244, 244, 244)))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -203,9 +227,15 @@ public class RegistroHuespedes extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jTextFieldElegHabitacion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabelElegHabitacion)
-                    .addComponent(jButtonElegHabitacion))
-                .addContainerGap(134, Short.MAX_VALUE))
+                    .addComponent(jLabelElegHabitacion))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 103, Short.MAX_VALUE)
+                .addComponent(jButtonRegistrar)
+                .addContainerGap())
+            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                    .addContainerGap(259, Short.MAX_VALUE)
+                    .addComponent(jButtonElegHabitacion)
+                    .addGap(124, 124, 124)))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -260,6 +290,26 @@ public class RegistroHuespedes extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jSpinnerDiasEstanciaStateChanged
 
+    private void jButtonElegHabitacionMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonElegHabitacionMouseReleased
+        // TODO add your handling code here:
+        try {
+                new DistribucionHabitaciones().setVisible(true);
+            } catch (SQLException ex) { }
+    }//GEN-LAST:event_jButtonElegHabitacionMouseReleased
+
+    private void jButtonRegistrarMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonRegistrarMouseReleased
+        // TODO add your handling code here:
+        String NomHuesped, DiasEstancia, PersonasExtra, Ciudad , Habitacion;
+        NomHuesped = jTextFieldNombreHuesped.getText();
+        DiasEstancia = String.valueOf((Integer)jSpinnerDiasEstancia.getValue()).trim();
+        PersonasExtra = String.valueOf((Integer)jSpinnerNumHospedados.getValue()-1).trim();//pasa el valor del spinner a string para pasarlo a la bd
+        Ciudad = jTextFieldCiudadHuesped.getText();
+        Habitacion = jTextFieldElegHabitacion.getText().trim();
+        Date FechaEntrada,FechaSalida;
+        
+     
+    }//GEN-LAST:event_jButtonRegistrarMouseReleased
+
     /**
      * @param args the command line arguments
      */
@@ -300,6 +350,7 @@ public class RegistroHuespedes extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JButton jButtonElegHabitacion;
+    private javax.swing.JButton jButtonRegistrar;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabelCiud;
     private javax.swing.JLabel jLabelCiud1;
