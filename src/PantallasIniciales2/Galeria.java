2da2965f8c02d470b626673a2d74dfd0e8cfb4ab
AdanSkandar <83788158+AdanSkandar@users.noreply.github.com>
2021-06-12 01:11:06 -0500

    Galeria imagenes completas y nombres

--- src/PantallasIniciales2/Galeria.java
+++ src/PantallasIniciales2/Galeria.java
@@ -6,6 +6,7 @@
 package PantallasIniciales2;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
@@ -17,7 +18,7 @@ import javax.swing.JLabel;
 public class Galeria extends JFrame implements ActionListener{
     
     JButton prev,next;
-    JLabel imagen;   
+    JLabel imagen, descripcionG;   
     ImageIcon icono;
     int contador = 1;
   
@@ -31,7 +32,7 @@ public class Galeria extends JFrame implements ActionListener{
          prev.addActionListener(this);
          
          imagen = new JLabel();
-          icono = new ImageIcon("src/1.jpg");
+         icono = new ImageIcon("src/1.jpg");
          imagen.setIcon(icono);
          imagen.setBounds(0,-60, 730, 600);
          
@@ -39,9 +40,19 @@ public class Galeria extends JFrame implements ActionListener{
          next.setBounds(645, 230, 60, 45);
          next.addActionListener(this);
          
+         descripcionG = new JLabel();
+         descripcionG.setBounds(280,515, 180, 70);
+         
+         descripcionG.setText("Habitacion sencilla");
+         descripcionG.setFont(new Font("Verdana", Font.BOLD, 14));
+         add(descripcionG);
+         
          add(prev);
          add(next);
-         add(imagen);       
+         add(imagen);      
+         
+         
+         
          
     }
 
@@ -50,7 +61,7 @@ public class Galeria extends JFrame implements ActionListener{
         
         if(e.getSource() == next){
             
-            if(contador >=10){
+            if(contador >=14){
             contador = 1;
             
             }else{
@@ -62,7 +73,7 @@ public class Galeria extends JFrame implements ActionListener{
         if(e.getSource() == prev){
             
            if(contador <= 1){
-               contador = 10;
+               contador = 14;
            }else{
            contador--;
            }
@@ -71,6 +82,23 @@ public class Galeria extends JFrame implements ActionListener{
         
         icono = new ImageIcon("src/"+contador+".jpg");
         imagen.setIcon(icono);
+        
+        
+        switch(contador){
+            case 1      : descripcionG.setText("Habitacion sencilla"); break;
+            case 2      : descripcionG.setText("Habitacion doble"); break;
+            case 3      : descripcionG.setText("Habitacion triple"); break;
+            case 4      : descripcionG.setText("Piscina"); break;
+            case 5      : descripcionG.setText("Spa"); break;
+            case 6      : descripcionG.setText("Restaurante"); break;
+            case 7      : descripcionG.setText("Restaurante afueras"); break;
+            case 8,9    : descripcionG.setText("Buffet"); break;
+            case 10,11  : descripcionG.setText("Bar"); break;
+            case 12,13  : descripcionG.setText("Cancha"); break;
+            case 14     : descripcionG.setText("Gimnasio"); break;
+                    
+        }
+        
         
     }
    
