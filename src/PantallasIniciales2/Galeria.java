0ff4a7ff675a634bcf09f30621dca38ea3dd2483
AdanSkandar <83788158+AdanSkandar@users.noreply.github.com>
2021-06-16 14:59:44 -0500

    Galeria con musica

--- src/PantallasIniciales2/Galeria.java
+++ src/PantallasIniciales2/Galeria.java
@@ -9,23 +9,56 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import proyectofinal.Login;
 
 
 public class Galeria extends JFrame implements ActionListener{
     
-    JButton prev,next;
+    JButton prev,next,pausa,salir,despausa;
     JLabel imagen, descripcionG;   
     ImageIcon icono;
     int contador = 1;
+    Clip clip;
   
     public Galeria(){
-         
+        this.setUndecorated(true);
+         try {
+            clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(new File("src/musica.wav")));
+            clip.start();
+        } catch (LineUnavailableException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedAudioFileException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
          
          setLayout(null);
+         
+         pausa = new JButton("pausar musica");
+         pausa.setBounds(225,0,110,40);
+         pausa.addActionListener(this);
+         
+         despausa = new JButton("reanudar musica");
+         despausa.setBounds(335,0,120,40);
+         despausa.addActionListener(this);
+         
+         salir = new JButton("X");
+         salir.setBounds(680,0,35,35);
+         salir.addActionListener(this);
          
          prev = new JButton("<");
          prev.setBounds(10,230,60,45);
@@ -47,12 +80,13 @@ public class Galeria extends JFrame implements ActionListener{
          descripcionG.setFont(new Font("Verdana", Font.BOLD, 14));
          add(descripcionG);
          
+         add(salir);
+         add(pausa);
+         add(despausa);
          add(prev);
          add(next);
          add(imagen);      
-         
-         
-         
+             
          
     }
 
@@ -97,6 +131,23 @@ public class Galeria extends JFrame implements ActionListener{
             case 12,13  : descripcionG.setText("Cancha"); break;
             case 14     : descripcionG.setText("Gimnasio"); break;
                     
+        }
+        
+        if(e.getSource() == pausa){   
+           clip.stop();
+           
+        }
+        if(e.getSource() == despausa){   
+           clip.start();
+           
+        }
+        
+        if(e.getSource() == salir){   
+            
+            
+           clip.stop();
+           clip.close();
+           this.dispose();
         }
         
         
